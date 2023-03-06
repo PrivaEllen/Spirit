63f7117ff75fac66f356fe9f21acf2cbd263c242
DaiTangoMne <mbuchnev@sfedu.ru>
2023-03-06 22:56:11 +0300

    added block on test page

--- client/src/pages/TestCreate.js
+++ client/src/pages/TestCreate.js
@@ -1,10 +1,14 @@
-import React from "react";
-import Header from "../components/tests/Header";
+import React from "react"
+import Header from "../components/tests/Header"
+import TestEdit from "../components/tests/TestEdit"
 
 export default function TestCreate() {
+
+    
     return (
         <div>
             <Header />
+            <TestEdit />
         </div>
     )
 }
