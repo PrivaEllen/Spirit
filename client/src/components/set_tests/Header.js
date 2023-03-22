c8b63015b30c6a341c181698062595b1336862f3
fenicotteri <96236303+fenicotteri@users.noreply.github.com>
2023-03-22 22:59:37 +0300

    added popovers but not completed

--- client/src/components/set_tests/Header.js
+++ client/src/components/set_tests/Header.js
@@ -1,8 +1,24 @@
 import React from "react";
 import SearchForm from "./SearchForm";
 import Avatar from '@mui/material/Avatar';
+import Popover from '@mui/material/Popover';
+
+
 
 export default function Header(){
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
     return(
         <header className="SetPageHeader">
         <div className="header__logo">
@@ -13,7 +29,29 @@ export default function Header(){
         <div className="header__account">
             <span className="header__account__name">Гаврилова Мария</span>
             <div className="avatar">
-                <Avatar sx={{ bgcolor: "#90CAF9", width: "49px", height: "49px" }}></Avatar>
+                <Avatar  onClick={handleClick} sx={{  width: "49px", height: "49px" }} />
+                <Popover
+                  id={id}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                sx={{ "& .MuiPopover-paper": {background: "none"} }}
+                >
+                <div className="popover-test">
+                  <div className="option__container">
+                  
+                  </div>
+
+                </div>
+              </Popover>
             </div>
         </div>
       </header>
--- client/src/components/set_tests/SearchForm.js
+++ client/src/components/set_tests/SearchForm.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import SmallIcon from '../tests/SmallIcon'
+import Popover from '@mui/material/Popover';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 
 export default function SearchForm() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <div  className="search">
       <form className="search__form">
@@ -11,11 +29,37 @@ export default function SearchForm() {
             className="search__input"/>
             <button className='close__button'></button>
         </form>
-        <SmallIcon svg = {
+        <SmallIcon onClick={handleClick} title="Тип отображаемых тестов" svg = {
                 <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 12H11V10H7V12ZM0 0V2H18V0H0ZM3 7H15V5H3V7Z"/>
                 </svg>                  
         }/>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        sx={{ "& .MuiPopover-paper": {background: "none"} }}
+      >
+        <div className="popover-test">
+          <div className="option__container">
+          <FormGroup>
+            <FormControlLabel control={<Checkbox defaultChecked />} label="Все тесты" className="option__text" />
+            <FormControlLabel control={<Checkbox />} label="Видимые" className="option__text"/>
+            <FormControlLabel control={<Checkbox />} label="Скрытые" className="option__text"/>
+          </FormGroup>
+          </div>
+        </div>
+      </Popover>
+        
     </div>
   )
 }
