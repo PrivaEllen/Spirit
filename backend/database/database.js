f369e791001a517da1a7b62967de355700916c10
fenicotteri <96236303+fenicotteri@users.noreply.github.com>
2023-05-22 12:26:33 +0300

    Trying to connect backend on my PC

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('Spirit', 'postgres', '123', {
+const sequelize = new Sequelize('Spirit', 'postgres', '12126', {
     host: 'localhost',
     dialect: 'postgres'
 });
