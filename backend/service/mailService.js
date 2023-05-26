297fedd042812588a9cedcade2aa6c1c0bcd9865
DaiTangoMne <mksim3uchnev@gmail.com>
2023-05-26 15:47:22 +0300

    .

--- backend/service/mailService.js
+++ backend/service/mailService.js
@@ -7,7 +7,7 @@ class mailService{
             secure: true,
             auth: {
                 user: process.env.SMTP_USER,
-                pass: process.env.SMTP_TOKEN
+                pass: process.env.SMTP_TOKEN,
             }
         })
     }
